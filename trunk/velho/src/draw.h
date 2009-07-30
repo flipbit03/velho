11b6abc8d549f5fa74be11ca015c71a6c1adb7fd
cadu <cadu@e635706e-8310-11de-bb68-1d15bb494b62>
2009-07-30 21:25:03 +0000

    Initial "Velho" commit. TODO: fix mingw/win32 Makefile

---
+++ trunk/velho/src/draw.h
@@ -0,0 +1,5 @@
+#include "SDL.h"
+
+void c_drawrectangle(SDL_Surface *surface, Uint16 x, Uint16 y, Uint16 rectx, Uint16 recty, Uint8 r, Uint8 g, Uint8 b);
+
+void c_clearscreen(SDL_Surface *surface, Uint8 r, Uint8 g, Uint8 b);
